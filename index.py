5e7b3853c650f1ca1b1a6bff6067878eb99eee91
Hari <155520906+ImamHM04@users.noreply.github.com>
2024-06-29 07:59:10 +0700

    success connect to db

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from dotenv import load_dotenv
 from connectors.mysql_connector import connection
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 
 load_dotenv()
 
@@ -8,4 +10,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+
+    Session = sessionmaker(connection)
+    with Session() as session:
+        session.execute(text("INSERT INTO user (username, email, password, role) VALUES('imamhm', 'f7hJ9@example.com', '12345', 'admin')"))
+        session.commit()
+
+    return "Inserted Successfully!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
